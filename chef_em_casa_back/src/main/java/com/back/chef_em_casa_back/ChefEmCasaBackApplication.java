f6ea7d8916398ea11fb7cdbf416716dfb95a8e7a
Maria Clara Fernandes <66803310+C-Fernandes@users.noreply.github.com>
2024-10-24 11:55:59 -0300

    Criação do back e do front

---
+++ chef_em_casa_back/src/main/java/com/back/chef_em_casa_back/ChefEmCasaBackApplication.java
@@ -0,0 +1,13 @@
+package com.back.chef_em_casa_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefEmCasaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChefEmCasaBackApplication.class, args);
+	}
+
+}
---
+++ chef_em_casa_back/src/test/java/com/back/chef_em_casa_back/ChefEmCasaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.chef_em_casa_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChefEmCasaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
