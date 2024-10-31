11bc6394e32a87514504e79f16dabab46990434c
Maria Clara Fernandes <66803310+C-Fernandes@users.noreply.github.com>
2024-10-30 22:01:04 -0300

    Ajustando Routes e primeiro Hello World

--- chef-em-casa-front/src/App.js
+++ chef-em-casa-front/src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
+import React from "react"; import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Olá chefe em casa
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          teste
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
+
   );
 }
 
