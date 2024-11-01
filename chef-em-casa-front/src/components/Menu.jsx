d7a2206162d0279616f9907e79ecfd97fc90558b
Maria Clara Fernandes <66803310+C-Fernandes@users.noreply.github.com>
2024-11-01 18:29:25 -0300

    Menu

---
+++ chef-em-casa-front/src/components/Menu.jsx
@@ -0,0 +1,28 @@
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400&display=swap"/>
+
+
+function Menu() {
+
+
+    return (
+        <div className="menu">
+            <h2>ChefEmCasa</h2>
+            <div id="nav">
+                <a href="/">Inicio</a>
+                <a href="">Explorar</a>
+                <a href="">Criar</a>
+            </div>
+            <div className="search-input">
+                <img src="./icons/searchIcon.png" alt="Icone de busca"/>
+                <input type="text" placeholder="Pesquise por uma receita"/>
+            </div>
+            <div className="userName">
+                <p>User Name</p>
+                <span className="initial">U</span>
+            </div>
+
+        </div>
+    );
+}
+
+export default Menu;
