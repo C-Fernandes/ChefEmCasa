5b6f4d19342f5d16cdacf5f27a51dd68b66559ac
Maria Clara Fernandes <66803310+C-Fernandes@users.noreply.github.com>
2024-12-11 18:57:46 -0300

    Atualizando login

--- chef_em_casa_back/src/main/java/com/back/chef_em_casa_back/repository/UserRepository.java
+++ chef_em_casa_back/src/main/java/com/back/chef_em_casa_back/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.back.chef_em_casa_back.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.back.chef_em_casa_back.entity.User;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User
-        , String>{}
+        , String>{
+
+    Optional<User> findByEmail(String email);}
--- chef_em_casa_back/src/main/java/com/back/chef_em_casa_back/utils/JwtUtil.java
+++ chef_em_casa_back/src/main/java/com/back/chef_em_casa_back/utils/JwtUtil.java
@@ -3,18 +3,19 @@ package com.back.chef_em_casa_back.utils;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
 import java.util.Date;
 import java.util.function.Function;
 
 @Component
 public class JwtUtil {
 
-    private static final String SECRET_KEY = "seu_segredo_super_secreto"; // Use algo mais seguro
-
+ private static final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
