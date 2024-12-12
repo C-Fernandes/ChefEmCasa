0de595be13dccbcfc543f08b06e3b65a2e60e40a
Pedro Rian <49865204+pedro-rian@users.noreply.github.com>
2024-12-12 17:01:15 -0300

    página de informações sobre receita (apenas front)

--- chef-em-casa-front/src/App.js
+++ chef-em-casa-front/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Explore from "./pages/Explore";
 import MyRecipes from "./pages/MyRecipes";
+import Recipe from "./pages/Recipe";
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/explorar' element={<Explore />} />
-        <Route path='/my-recipes' element={<MyRecipes/>}/>
+        <Route path='/my-recipes' element={<MyRecipes />} />
+        <Route path='/recipe' element={<Recipe />} />
       </Routes>
     </Router>
 
