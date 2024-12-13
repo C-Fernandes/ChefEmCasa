8f40a73aad9a50fb668d8dc7bd31ff58e60a2a1b
Maria Clara Fernandes <66803310+C-Fernandes@users.noreply.github.com>
2024-12-13 20:07:14 -0300

    Recipe Controller

--- chef_em_casa_back/src/main/java/com/back/chef_em_casa_back/controller/RecipeController.java
+++ chef_em_casa_back/src/main/java/com/back/chef_em_casa_back/controller/RecipeController.java
@@ -14,13 +14,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.back.chef_em_casa_back.dto.RecipeDTO;
-import com.back.chef_em_casa_back.entity.Ingredient;
 import com.back.chef_em_casa_back.entity.Recipe;
 import com.back.chef_em_casa_back.service.RecipeService;
 
