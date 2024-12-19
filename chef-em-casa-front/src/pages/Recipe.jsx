299a17ed502c1251d81a5b07ce1604c636ce6fbd
Pedro Rian <49865204+pedro-rian@users.noreply.github.com>
2024-12-19 11:11:59 -0300

    campos de comentarios e detalhes do front de receita

--- chef-em-casa-front/src/pages/Recipe.jsx
+++ chef-em-casa-front/src/pages/Recipe.jsx
@@ -3,7 +3,7 @@ import Menu from "../components/Menu";
 import Footer from "../components/footer";
 
 
-function Recipes() {
+function Recipes(props) {
     return (
         <>
             <Menu />
@@ -21,12 +21,8 @@ function Recipes() {
                     <img className="imagem-informacoes" src="/images/torta.jpeg" alt="" />
                     <h1>Torta de Chocolate</h1>
                     <div className="estrelas">
-                        <span style={{ color: "#FFA834" }}>4.8</span>
-                        <img className="star" src="/icons/estrela.png" alt="" />
-                        <img className="star" src="/icons/estrela.png" alt="" />
-                        <img className="star" src="/icons/estrela.png" alt="" />
-                        <img className="star" src="/icons/estrela.png" alt="" />
-                        <img className="star" src="/icons/estrela.png" alt="" />
+                        <svg className="heart" xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FF5D5D"><path d="m480-131-54-48.5q-99.27-89.57-164.13-154.54Q197-399 158.72-450.4q-38.29-51.4-53.5-94.48Q90-587.96 90-633q0-91.01 61-152 60.99-61 152-61 51.29 0 97.64 22Q447-802 480-761q34.5-41 80-63t97-22q91.01 0 152 61 61 60.99 61 152 0 45.04-15.22 88.12-15.21 43.08-53.5 94.48Q763-399 698.13-334.04 633.27-269.07 534-179.5L480-131Zm0-101q94-85 155-145.5T731.5-483q35.5-45 49.5-80.18 14-35.18 14-69.86 0-59.46-39.36-98.71Q716.28-771 657.25-771q-46.25 0-86 26.5t-56.25 69h-70q-16.5-42.5-56.25-69t-86-26.5q-59.03 0-98.39 39.25T165-633.04q0 34.68 14 69.86T228.5-483Q264-438 325-377.5T480-232Zm0-269.5Z" /></svg>{props.num_likes}
+                        <span style={{ color: "rgb(255, 93, 93)" }}>Favoritar</span>
                         <img className="clock" src="/icons/relogio.png" alt="" />
                         <span style={{ color: "#60E3C4" }}>40 min</span>
                     </div>
@@ -54,6 +50,46 @@ function Recipes() {
                         Receita criada por
                         <div className="circulo"></div>
                         <b>Nome da Pessoa</b>
+                    </div>
+                    <div className="comentarios-receita">
+                        <h2 style={{marginBottom: "20px"}}>3 Comentários</h2>
+                        <div className="comentario-pessoal">
+                            <div className="circulo"></div>
+                            <div className="espaco-comentario">
+                                <input className="input-comentario" type="text" placeholder="Faça um comentário" />
+                                <div>
+                                    <button className="btn-comentar">Comentar</button>
+                                    <button className="btn-cancelar">Cancelar</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="comentario-div">
+                            <div className="circulo"></div>
+                            <div className="comentario">
+                                <h4 className="texto-comentario">Nome do usuário 1</h4>
+                                <div>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="comentario-div">
+                            <div className="circulo"></div>
+                            <div className="comentario">
+                                <h4 className="texto-comentario">Nome do usuário 2</h4>
+                                <div>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque consectetur, lectus nec malesuada efficitur, eros felis rutrum felis, pulvinar semper nisi est interdum nibh. Vivamus tempus odio sed nibh aliquam vehicula. Aliquam eu gravida urna. Phasellus non sollicitudin neque. Integer volutpat ultricies gravida. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="comentario-div">
+                            <div className="circulo"></div>
+                            <div className="comentario">
+                                <h4 className="texto-comentario">Nome do usuário 3</h4>
+                                <div>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque consectetur, lectus nec malesuada efficitur, eros felis rutrum felis, pulvinar semper nisi est interdum nibh.</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
